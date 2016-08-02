c2719ad47f6c9f34901aca98877df18436b74716
liyipeng <summertree.lee@gmail.com>
2016-08-02 11:48:39 +0800

    use Element Buffer Object (索引缓冲对象)

--- src/HelloWindow/TriangleMe.cpp
+++ src/HelloWindow/TriangleMe.cpp
@@ -97,25 +97,40 @@ int main()
     GLfloat vertices[] = {
         -0.5, -0.5, 0.0,
         0.5, -0.5, 0.0,
-        0.0, 0.5, 0.5
+        0.5, 0.5, 0.0,
+        -0.5, 0.5, 0.0
+    };
+    
+    GLuint indices[] = {
+        0, 1, 3,
+        1, 2, 3
+        
     };
     
     
-    GLuint VBO, VAO;
+    GLuint VBO, VAO, EBO;
     
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     
     glEnableVertexAttribArray(0);
     
     glBindVertexArray(0);
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    
     
     while (!glfwWindowShouldClose(window)) {
         
@@ -129,7 +144,9 @@ int main()
         
         glBindVertexArray(VAO);
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glBindVertexArray(0);
         
